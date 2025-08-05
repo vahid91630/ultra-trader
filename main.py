c987143a47bb1898d17a7cdd8b28d6e6fec64c14
vahid <vahid91630@gmail.com>
2025-08-05 16:16:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import time
+
 if __name__ == "__main__":
     print("✅ Ultra Trader Booted")
-    input("✅ Ultra Trader is running. Press Enter to exit.")
+    print("✅ Ultra Trader is running. Press CTRL+C to stop.")
+    while True:
+        time.sleep(60)
