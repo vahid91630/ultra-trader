b5aa520e255d1229dfe94979fec0e0fa0f5624c9
vahid <vahid91630@gmail.com>
2025-08-06 00:09:15 +0330

    Update mongo_data_store.py

--- infra/mongo_data_store.py
+++ infra/mongo_data_store.py
@@ -5,10 +5,10 @@ from pymongo.server_api import ServerApi
 def connect_to_mongodb():
     uri = os.getenv("MONGODB_URI")
     if not uri:
-        print("⚠️ MONGO_URI env var not set – using Safe Mode")
+        print("⚠️ MONGODB_URI not set – using Safe Mode")
         return None
-    client = MongoClient(uri, server_api=ServerApi('1'))
     try:
+        client = MongoClient(uri, server_api=ServerApi('1'))
         client.admin.command('ping')
         print("✅ Connected to MongoDB:", uri.split("/")[-1].split("?")[0])
         return client
