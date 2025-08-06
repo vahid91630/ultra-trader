5de5ebc2a36d45456f20d6f44f88b824adb6f62d
vahid <vahid91630@gmail.com>
2025-08-06 05:50:01 +0330

    Update main.py

--- ultra_dashboard/main.py
+++ ultra_dashboard/main.py
@@ -1,7 +1,5 @@
 import os
-import streamlit as st
-from infra.api_manager import APIManager
 
 if __name__ == "__main__":
     print("✅ Ultra Trader Dashboard Booted")
-    os.system("streamlit run ultra_dashboard/dashboard.py")
+    os.system("streamlit run ultra_dashboard/dashboard.py --server.enableCORS false --server.port $PORT")
