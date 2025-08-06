72e12c08ae62dda518a88f8bdd40596a05bccf3e
vahid <vahid91630@gmail.com>
2025-08-06 16:17:52 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-streamlit run streamlit_app.py --server.port 8080 --server.address 0.0.0.0
+from binance_connector import get_live_price
+import streamlit as st
+
+st.title("💹 قیمت لحظه‌ای")
+
+price_data = get_live_price("BTCUSDT")
+st.metric("BTC/USDT", f"${price_data['price']}")
